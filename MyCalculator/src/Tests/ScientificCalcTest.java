a59b6fa1f80f55cc64eb55a04e2919bdf77ff507
killianormo <killianormond@gmail.com>
2013-04-11 13:20:55 +0100

    Branch setup

---
+++ MyCalculator/src/Tests/ScientificCalcTest.java
@@ -0,0 +1,29 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ScientificCalcTest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
